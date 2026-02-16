0b6d624f951e2f20911dcc91a3f3653165cc2f5c
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-16 18:01:46 +0300

    Update zervo.py

--- src/zervo.py
+++ src/zervo.py
@@ -6,7 +6,8 @@ class Zervo:
 		self.token = None
 		self.user_id = None
 		self.language = language
-		self.headers = {
+		self.session = requests.Session()
+		self.session.headers = {
 			"User-Agent": "Dart/2.18 (dart:io)",
 			"language": self.language
 		}
@@ -28,10 +29,9 @@ class Zervo:
 			"birthday": birthday,
 			"sex": sex
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/v3/app-user/register",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 
 	def register_with_google(
 			self,
@@ -46,10 +46,9 @@ class Zervo:
 			"source": "google",
 			"uid": google_id
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/v2/app-user/register",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 
 	def login(
 			self,
@@ -59,14 +58,13 @@ class Zervo:
 			"email": email,
 			"password": password
 		}
-		response = requests.post(
+		response = self.session.post(
 			f"{self.api}/pinpon-app-auth/v3/auth/login/email",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 		if "token" in response["data"]["pinponToken"]:
 			self.user_id = response["data"]["appUserId"]
 			self.token = response["data"]["pinponToken"]["token"]
-			self.headers["pinpon-auth"] = self.token
+			self.session.headers["pinpon-auth"] = self.token
 		return response
 
 
@@ -75,14 +73,13 @@ class Zervo:
 			"source": "google",
 			"id": google_id
 		}
-		response = requests.post(
+		response = self.session.post(
 			f"{self.api}/pinpon-app-auth/auth/login/source",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 		if "token" in response["data"]["pinponToken"]:
 			self.user_id = response["data"]["appUserId"]
 			self.token = response["data"]["pinponToken"]["token"]
-			self.headers["pinpon-auth"] = self.token
+			self.session.headers["pinpon-auth"] = self.token
 		return response
 	
 	def request_verification_code(self, email: str) -> dict:
@@ -91,10 +88,9 @@ class Zervo:
 			"type": 2,
 			"language": self.language
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/v2/app-user/mail",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 
 	def comment(
 			self,
@@ -106,10 +102,9 @@ class Zervo:
 			"comment": comment,
 			"images": images
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/app-comment/save",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 	
 	def like_post(self, post_id: int, like_status: int = 1) -> dict:
 		"""
@@ -122,10 +117,9 @@ class Zervo:
 			"type": 1,
 			"likeStatus": like_status
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/app-like/like/status",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 	
 	def send_friend_request(
 			self,
@@ -137,10 +131,9 @@ class Zervo:
 		"receiveUserId": user_id,
 		"source": 1
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/app-friend-req/save",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 	
 	def block_user(
 			self,
@@ -150,111 +143,95 @@ class Zervo:
 			"friendUserId": user_id,
 			"isFriend": is_friend
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/app-friend/block",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 		
 	def unblock_user(self, user_id: int) -> dict:
 		data = {
 			"friendUserId": user_id
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/app-friend/unblock",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 		
 	def get_album_info(
 			self,
 			album_id: int,
 			limit: int = 10) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/v2/app-meme/page?limit={limit}&albumId={album_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/v2/app-meme/page?limit={limit}&albumId={album_id}").json()
 		
 	def get_post_comments(
 			self,
 			post_id: int,
 			size: int = 10,
 			current: int = 1) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/app-comment/list?contentId={post_id}&size={size}&current={current}&commentId",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/app-comment/list?contentId={post_id}&size={size}&current={current}&commentId").json()
 		
 	def get_app_roles(self) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-auth/pinpon-app-system/v2/app-role/default",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-auth/pinpon-app-system/v2/app-role/default").json()
 		
 	def get_friends_list(self) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/app-friend/list",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/app-friend/list").json()
 		
 	def get_role_info(self, role_id: int) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/app-role/detail?roleId={role_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/app-role/detail?roleId={role_id}").json()
 		
 	def get_recent_posts(
 			self,
 			size: int = 10,
 			current: int = 1) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/app-content/query?size={size}&current={current}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/app-content/query?size={size}&current={current}").json()
 		
 	def get_user_oc_list(self, user_id: int) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/app-oc/list?appUserId={user_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/app-oc/list?appUserId={user_id}").json()
 		
 	def get_user_info(self, user_id: int) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/v2/app-user/detail?appUserId={user_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/v2/app-user/detail?appUserId={user_id}").json()
 		
 	def get_user_stats(self, user_id: int) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/app-content/stats?appUserId={user_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/app-content/stats?appUserId={user_id}").json()
 		
 	def get_user_posts(self, user_id: int, size: int = 10) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/app-content/list?appUserId={user_id}&contentId&size={size}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/app-content/list?appUserId={user_id}&contentId&size={size}").json()
 		
 	def get_user_albums(self, user_id: int) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/v2/app-album/list?appUserId={user_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/v2/app-album/list?appUserId={user_id}").json()
 		
 	def get_recommended_channels(
 			self,
 			current: int = 1,
 			size: int = 10,
 			name: str = None) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/v2/app-channel/recommend/tag?current={current}&size={size}&name={name}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/v2/app-channel/recommend/tag?current={current}&size={size}&name={name}").json()
 		
 	def join_channel(self, channel_id: int) -> dict:
 		data = {
 			channel_id
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/v2/app-channel-user/save",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 		
 	def leave_channel(self, channel_id: int) -> dict:
 		data = {
 			"channelId": channel_id
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/v2/app-channel-user/remove",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 		
 	def invite_to_channel(
 			self,
@@ -267,75 +244,65 @@ class Zervo:
 			],
 			"channelId": channel_id
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/app-channel-user/invite",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 		
 	def get_channel_info(self, channel_id: int) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/v2/app-channel/detail?channelId={channel_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/v2/app-channel/detail?channelId={channel_id}").json()
 		
 	def get_channel_users(
 			self,
 			channel_id: int,
 			current: int = 1,
 			size: int = 10) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/app-channel-user/{channel_id}?current={current}&size={size}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/app-channel-user/{channel_id}?current={current}&size={size}").json()
 		
 	def get_channel_moderators(self, channel_id: int) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/app-channel-user/moderator?channelId={channel_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/app-channel-user/moderator?channelId={channel_id}").json()
 		
 	def get_current_session(self) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/app-user/current-detail",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/app-user/current-detail").json()
 		
 	def get_recommended_users(
 			self,
 			current: int = 1,
 			size: int = 10,
 			name: str = None) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/v4/app-recommend?size={size}&current={current}&name={name}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/v4/app-recommend?size={size}&current={current}&name={name}").json()
 		
 	def get_recommended_albums(
 			self,
 			current: int = 1,
 			size: int = 10,
 			name: str = None) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/app-album/recommend?current={current}&size={size}&name={name}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/app-album/recommend?current={current}&size={size}&name={name}").json()
 		
 	def get_recommended_all(
 			self,
 			current: int = 1,
 			size: int = 10,
 			name: str = None) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/v4/app-recommend/all?current={current}&size={size}&name={name}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/v4/app-recommend/all?current={current}&size={size}&name={name}").json()
 		
 	def get_app_tags(self) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/app-tag/list",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/app-tag/list").json()
 		
 	def get_channel_blacklist(
 			self,
 			channel_id: int,
 			current: int = 1,
 			size: int = 10) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/app-group-black/list?current={current}&size={size}&groupId={channel_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/app-group-black/list?current={current}&size={size}&groupId={channel_id}").json()
 		
 	def create_channel(
 			self,
@@ -351,10 +318,9 @@ class Zervo:
 			"name": title,
 			"tagList": tag_list
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/v3/app-channel/chat/save",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 		
 	def edit_channel(
 			self,
@@ -369,19 +335,17 @@ class Zervo:
 			data["bio"] = description
 		if language:
 			data["language"] = language
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/v2/app-channel/update",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 		
 	def delete_channel(self, channel_id: int) -> dict:
 		data = {
 			"channelId": channel_id
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/v2/app-channel/remove",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 		
 	def report_user(
 			self,
@@ -402,15 +366,13 @@ class Zervo:
 			"type": type,
 			"url": url
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/app-report/save",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 		
 	def get_friend_requests(self) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/app-friend-req/list",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/app-friend-req/list").json()
 		
 	def edit_profile(
 			self,
@@ -430,20 +392,17 @@ class Zervo:
 			data["sex"] = gender
 		if role_id:
 			data["appRoleId"] = role_id
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/app-user/update",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 		
 	def edit_profile_id(self, id: str) -> dict:
-		return requests.post(
-			f"{self.api}/pinpon-app-system/app-user/update/userId?userId={id}",
-			headers=self.headers).json()
+		return self.session.post(
+			f"{self.api}/pinpon-app-system/app-user/update/userId?userId={id}").json()
 		
 	def get_owned_app_roles(self) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/v2/app-role/owned-list-all",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/v2/app-role/owned-list-all").json()
 		
 	def create_post(
 			self,
@@ -455,15 +414,13 @@ class Zervo:
 			"content": content,
 			"images": images
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/app-content/save",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 		
 	def delete_post(self, post_id: int) -> dict:
-		return requests.post(
-			f"{self.api}/pinpon-app-system/app-content/remove?contentId={post_id}",
-			headers=self.headers).json()
+		return self.session.post(
+			f"{self.api}/pinpon-app-system/app-content/remove?contentId={post_id}").json()
 		
 	def create_oc(
 			self,
@@ -489,51 +446,44 @@ class Zervo:
 				}
 			]
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/app-oc/save",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 		
 	def delete_oc(self, oc_id: int) -> dict:
 		data = {
 			"ids": oc_id
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/app-oc/remove",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 		
 	def get_joined_channels(self) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/v3/app-channel/chat/list",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/v3/app-channel/chat/list").json()
 		
 	def get_game_maps(
 			self,
 			current: int = 1,
 			page: int = 10) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/game-map/list?current={current}&page={page}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/game-map/list?current={current}&page={page}").json()
 		
 	def get_points(self) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/app-user/point",
-			headers=self.headers).json
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/app-user/point").json
 		
 	def spin_gachapon(self) -> dict:
-		return requests.post(
-			f"{self.api}/pinpon-app-system/app-library/draw",
-			headers=self.headers).json()
+		return self.session.post(
+			f"{self.api}/pinpon-app-system/app-library/draw").json()
 
 	def choose_avatar(self, template_id: int) -> dict:
 		data = {
 			"avatarTemplateId": template_id
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/app-avatar/choose/template",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 
 	def get_recommended_rp(
 			self,
@@ -543,12 +493,10 @@ class Zervo:
 			"current": current,
 			"size": size
 		}
-		return requests.post(
+		return self.session.post(
 			f"{self.api}/pinpon-app-system/v5/app-recommend/rp",
-			data=data,
-			headers=self.headers).json()
+			data=data).json()
 
 	def get_oc_current_list(self) -> dict:
-		return requests.get(
-			f"{self.api}/pinpon-app-system/app-oc/current/list",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pinpon-app-system/app-oc/current/list").json()
